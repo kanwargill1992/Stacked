3fc95d92541b15dfededd84e6e87d4094964e8aa
Kanwar gill <gill.nihal@live.com>
2020-09-02 14:23:15 -0700

    server works

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      db,
+
+      { useUnifiedTopology: true, useNewUrlParser: true }
+    );
+    console.log("Connected.....");
+  } catch (err) {
+    console.log(err.message);
+    //Process is exited
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+connectDB();
 app
   .get("/", (req, res) => res.send("Running"))
 
