211f1c7f2a99d08cc84f453eed8d5c7f0a44377e
Kanwar gill <gill.nihal@live.com>
2020-10-23 10:38:32 -0400

    user auth

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,6 +5,8 @@ import {
   REGISTER_SUCCESS,
   USER_LOADED,
   AUTH_ERROR,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -28,7 +30,6 @@ export const loadUser = () => async (dispatch) => {
 };
 
 //Register User
-
 export const register = ({ name, email, password }) => async (dispatch) => {
   const config = {
     headers: {
@@ -54,6 +55,36 @@ export const register = ({ name, email, password }) => async (dispatch) => {
 
     dispatch({
       type: REGISTER_FAIL,
+    });
+  }
+};
+
+//Login User
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post("/api/auth", body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: LOGIN_FAIL,
     });
   }
 };
--- config/db.js
+++ config/db.js
@@ -14,7 +14,6 @@ const connectDB = async () => {
         useFindAndModify: false,
       }
     );
-    console.log("MongoDB Connected.....");
   } catch (err) {
     console.log(err.message);
     //Process is exited
