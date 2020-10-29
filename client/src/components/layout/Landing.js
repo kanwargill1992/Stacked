f8c42988c1eb359eef08d3fe03b8350931ba2708
Kanwar gill <gill.nihal@live.com>
2020-10-29 18:26:41 -0400

    landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
@@ -6,20 +7,29 @@ const Landing = () => {
     <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
-          <h1 className="x-large">Welcome to Stacked</h1>
-          <p className="lead">For Developers by Developers</p>
-          <div className="buttons">
-            <Link to="/register" className="btn btn-primary">
-              Sign Up
-            </Link>
-            <Link to="/login" className="btn">
-              Login
-            </Link>
-          </div>
+          <Wrapper>
+            <h1 className="x-large">Welcome to Stacked</h1>
+            <p className="lead">For Developers by Developers</p>
+            <div className="buttons">
+              <Link to="/register" className="btn btn-primary">
+                Sign Up
+              </Link>
+              <Link to="/login" className="btn">
+                Login
+              </Link>
+            </div>
+          </Wrapper>
         </div>
       </div>
     </section>
   );
 };
+
+const Wrapper = styled.div`
+  background-color: #0b3142;
+  border: 1px solid #5db7de;
+  padding: 1.5rem;
+  opacity: 0.9;
+`;
 
 export default Landing;
