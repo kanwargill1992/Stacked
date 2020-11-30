9f5914932ee73242aff522cea3c134d42ccd9adc
Kanwar gill <gill.nihal@live.com>
2020-11-29 22:47:24 -0500

    About Profile

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ProfileAbout = ({ profile: {} }) => {
+  return <div></div>;
+};
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
+
+export default ProfileAbout;
